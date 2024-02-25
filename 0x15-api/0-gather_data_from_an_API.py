740d23dd00d6467172120663ceca2d283572635b
root <tefeglory.official@gmail.com>
2024-02-25 16:15:26 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-'''
-Gather data from an API  for a given employee ID
-'''
-
-import re
+"""Gather data from an API  for a given employee ID"""
 import requests
 import sys
+
 
 BASE_URL = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
+        id = int(sys.argv[1])
+        if id:
             employee = requests.get('{}/users/{}'.format(BASE_URL, id)).json()
+            employee_name = employee.get('name')
             tasks = requests.get('{}/todos?userId={}'
                                  .format(BASE_URL, id)).json()
             completed_tasks = [task for task in tasks if task.get("completed")]
             print('Employee {} is done with tasks({}/{}):'
-                  .format(employee.get("name"), len(completed_tasks),
-                                                len(tasks)))
+                  .format(employee_name, len(completed_tasks),
+                          len(tasks)))
             if len(completed_tasks) > 0:
                 for task in completed_tasks:
                     print('\t {}'.format(task.get("title")))
