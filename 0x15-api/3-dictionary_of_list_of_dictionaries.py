f11f592ad8b73134bcd15787778a38686a2c06fb
root <tefeglory.official@gmail.com>
2024-02-25 23:16:57 +0100

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Exports todo list of all employees in JSON format
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            user.get("id"): [{
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username")
+            } for todo in requests.get(url + "todos",
+                                    params={"userId": user.get("id")}).json()]
+            for user in users}, jsonfile)
