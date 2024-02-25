afa0926f131d95e9b6411b758bf43096f0c5cd64
root <tefeglory.official@gmail.com>
2024-02-25 17:11:25 +0100

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Export api to csv"""
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    request_url = 'https://jsonplaceholder.typicode.com/users'
+    user = requests.get('{}/{}'.format(request_url, user_id)).json()
+    user_name = user.get('username')
+    tasks = requests.get('{}/{}/todos'.format(request_url, user_id)).json()
+
+    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['User ID', 'Username', 'Completed', 'Task Title'])
+        for task in tasks:
+            completed = str(task.get('completed'))
+            task_title = str(task.get('title'))
+            csv_writer.writerow([str(user_id),
+                                 str(user_name), completed, task_title])
