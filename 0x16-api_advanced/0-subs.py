1f0dd5800cf68366bf548b51fa2b342339aacd66
root <tefeglory.official@gmail.com>
2024-05-09 22:56:12 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,10 +16,3 @@ def number_of_subscribers(subreddit):
         if subs:
             return res.get("subscribers")
         return 0
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
