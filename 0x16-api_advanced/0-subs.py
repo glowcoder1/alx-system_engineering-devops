9ca3f35424d4df894849cef284730b6448379ccc
root <tefeglory.official@gmail.com>
2024-05-09 22:46:12 +0100

    task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" returns subscripber count"""
+
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    if subreddit is None:
+        return None
+    req = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                       headers={"User-Agent": "Custom"})
+    if req.status_code == 200:
+        res = req.json().get("data")
+    return res.get("subscribers")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
